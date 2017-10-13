b09ee23fd861b0ebcf56ebad52b959b5d1f012bf
liujunzhang007 <liujunzhang007@sina.cn>
2017-10-13 15:49:13 +0800

    Release 1.0.1

--- PublicTools/PublicTools.framework/Headers/TextFontFit.h
+++ PublicTools/PublicTools.framework/Headers/TextFontFit.h
@@ -1,15 +1,16 @@
 //
-//  UIColor+ColorTransfer.h
+//  TextFontFit.h
 //  TourWay
 //
 //  Created by WuTongAlvin on 11/4/15.
 //  Copyright © 2015 OneThousandandOneNights. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-@interface UIColor (ColorTransfer)
+@interface TextFontFit : NSObject
 
-+ (UIColor *)colorTransferToRGB:(NSString *)color;
++ (CGFloat)fontWithiPhone6PlusFont:(CGFloat)myFont;
 
 @end
